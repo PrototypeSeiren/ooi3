7a3c9e2c43baf5049a8cc5dd9ed1b0d1aa42cafd
PrototypeSeiran <1020612624@qq.com>
2018-07-05 23:34:09 +0800

    Update config.py

--- base/config.py
+++ base/config.py
@@ -11,3 +11,5 @@ base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 template_dir = os.path.join(base_dir, 'templates')
 static_dir = os.path.join(base_dir, 'static')
 kcs_dir = os.path.join(base_dir, '_kcs')
+
+white_list = base_dir+'/WhiteList.txt'
